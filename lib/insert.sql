2e9ade4156e2e424f464474e7f59e0ee6078a17e
Lrd134 <larryc3200@gmail.com>
2021-04-25 17:28:16 -0400

    commit tree 921c432fd8e198956d644b3c69f20522b42bedcd

---
+++ lib/insert.sql
@@ -0,0 +1,70 @@
+INSERT INTO users VALUES (1, "Hannah", 23);
+INSERT INTO users VALUES (2, "Bananah", 24);
+INSERT INTO users VALUES (3, "Hanah", 23);
+INSERT INTO users VALUES (4, "Haah", 23);
+INSERT INTO users VALUES (5, "Bannah", 21);
+INSERT INTO users VALUES (6, "Banana", 21);
+INSERT INTO users VALUES (7, "Bobana", 21);
+INSERT INTO users VALUES (8, "Fofana", 21);
+INSERT INTO users VALUES (9, "Alafo", 23);
+INSERT INTO users VALUES (10, "Cobana", 21);
+INSERT INTO users VALUES (11, "Twist", 21);
+INSERT INTO users VALUES (12, "Sj", 22);
+INSERT INTO users VALUES (13, "JD", 21);
+INSERT INTO users VALUES (14, "Mike", 23);
+INSERT INTO users VALUES (15, "Tike", 23);
+INSERT INTO users VALUES (16, "Gary", 24);
+INSERT INTO users VALUES (17, "Jim", 27);
+INSERT INTO users VALUES (18, "Steve", 26);
+INSERT INTO users VALUES (19, "Jerry", 25);
+INSERT INTO users VALUES (20, "Hannibal", 22);
+
+
+INSERT INTO projects VALUES (1, "Husky", "Crpyto", 3000000000, "02/14/21", "12/25/21");
+INSERT INTO projects VALUES (2, "BTC", "Crpyto", 2000000000000, "01/01/13", "4/25/21");
+INSERT INTO projects VALUES (3, "HTR", "Crpyto", 90000000000, "01/01/20", "12/31/25");
+INSERT INTO projects VALUES (4, "IDEA", "Crpyto", 5600000000, "02/14/21", "12/22/34");
+INSERT INTO projects VALUES (5, "New Flip Phone", "Gadget", 3000000, "02/14/21", "10/01/21");
+INSERT INTO projects VALUES (6, "Non-stick Frying Pan", "Gadget", 30000, "02/14/21", "08/01/21");
+INSERT INTO projects VALUES (7, "Typrohen", "Medication", 30000000, "02/14/21", "12/24/21");
+INSERT INTO projects VALUES (8, "New Phone", "Gadget", 300000, "02/10/21", "10/04/21");
+INSERT INTO projects VALUES (9, "Frying Pan", "Gadget", 300000, "02/08/22", "05/04/23");
+INSERT INTO projects VALUES (10, "Tyhen", "Medication", 3000000, "02/14/21", "12/24/21");
+
+
+
+INSERT INTO pledges VALUES (1, 8000, 8, 1);
+INSERT INTO pledges VALUES (2, 1000, 3, 1);
+INSERT INTO pledges VALUES (3, 19000, 9, 1);
+INSERT INTO pledges VALUES (4, 8000, 2, 2);
+INSERT INTO pledges VALUES (5, 1000, 7, 2);
+INSERT INTO pledges VALUES (6, 19000, 1, 2);
+INSERT INTO pledges VALUES (7, 8000, 8, 3);
+INSERT INTO pledges VALUES (8, 1000, 4, 3);
+INSERT INTO pledges VALUES (9, 19000, 9, 3);
+INSERT INTO pledges VALUES (10, 8000, 20, 4);
+INSERT INTO pledges VALUES (11, 1000, 6, 4);
+INSERT INTO pledges VALUES (12, 19000, 10, 4);
+INSERT INTO pledges VALUES (13, 8000, 19, 5);
+INSERT INTO pledges VALUES (14, 1000, 3, 5);
+INSERT INTO pledges VALUES (15, 19000, 18, 5);
+INSERT INTO pledges VALUES (16, 8000, 2, 6);
+INSERT INTO pledges VALUES (17, 1000, 7, 6);
+INSERT INTO pledges VALUES (18, 19000, 17, 6);
+INSERT INTO pledges VALUES (19, 8000, 8, 7);
+INSERT INTO pledges VALUES (20, 1000, 4, 7);
+INSERT INTO pledges VALUES (21, 19000, 15, 7);
+INSERT INTO pledges VALUES (22, 8000, 3, 8);
+INSERT INTO pledges VALUES (23, 1000, 6, 8);
+INSERT INTO pledges VALUES (24, 19000, 16, 8);
+INSERT INTO pledges VALUES (25, 8000, 8, 9);
+INSERT INTO pledges VALUES (26, 1000, 4, 9);
+INSERT INTO pledges VALUES (27, 19000, 14, 9);
+INSERT INTO pledges VALUES (28, 8000, 11, 10);
+INSERT INTO pledges VALUES (29, 1000, 12, 10);
+INSERT INTO pledges VALUES (30, 19000, 13, 10);
+
+
+
+
+-- INSERT INTO VALUES ();
